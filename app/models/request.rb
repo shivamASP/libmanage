7a252d71ba2060639574144704e938d020bd0ce7
Shivam Kumar <shivam.kumar@mountblue.tech>
2021-04-21 14:39:27 +0530

    Update model validations for presence true

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ApplicationRecord
-  validates :title, length: { minimum: 2 }
+  validates :title, length: { minimum: 2 }, presence: true
   validates :author, length: { minimum: 2, maximum: 20 }, presence: true
   validates :published_in, numericality: {less_than_or_equal_to: 2020, greater_than_or_equal_to: 1800, allow_nil: true}
   validates :volume, numericality: {greater_than_or_equal_to: 1, allow_nil: true}
