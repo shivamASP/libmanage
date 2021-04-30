b32e0d9b521c3b8c77a673f8598075de48b4e332
Shivam Kumar <shivam.kumar@mountblue.tech>
2021-04-30 18:27:04 +0530

    Add capybara and simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'devise'
 # require 'support/factory_bot'
 # require 'support/controller_macros'
 Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
