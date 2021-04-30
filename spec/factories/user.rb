f13930c628a8d30ee086ad8d8ae69b2b78d88a70
Shivam Kumar <shivam.kumar@mountblue.tech>
2021-05-01 02:13:04 +0530

    Add admin trait to user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :random_user, class: User do
     email { Faker::Internet.safe_email }
     password { Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3, min_numeric: 3) }
-    factory :admin do
+    trait :admin do
       admin { true }
     end
   end
