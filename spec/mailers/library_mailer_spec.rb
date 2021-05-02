de7e1befc255928732ba75784fc7393a92cee856
Shivam Kumar <shivam.kumar@mountblue.tech>
2021-05-03 02:37:51 +0530

    Revert "Remove reminder example from librarymailer_spec"
    This reverts commit 784e8610ec9d5e2827d1fea8b139c0cbaf0f99b7.

--- spec/mailers/library_mailer_spec.rb
+++ spec/mailers/library_mailer_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe LibraryMailer, type: :mailer do
   describe 'issue' do
     let(:bookissue) { create(:bookissue) }
+
     let(:mail) { LibraryMailer.issue(bookissue) }
 
     it 'renders the headers' do
@@ -16,6 +17,25 @@ RSpec.describe LibraryMailer, type: :mailer do
     it 'renders the body' do
       user = User.find(bookissue.user_id)
       expect(mail.body.encoded).to match("Hi! #{user.email}")
+    end
+  end
+end
+
+RSpec.describe LibraryMailer, type: :mailer do
+  describe 'return' do
+    let(:bookissue) { create(:bookissue) }
+    let(:mail) { LibraryMailer.return(bookissue) }
+
+    it 'renders the headers' do
+      book = Book.find(bookissue.book_id)
+      user = User.find(bookissue.user_id)
+      expect(mail.subject).to eq("Congrats! Book #{book.title} successfully returned")
+      expect(mail.to).to eq([user.email.to_s])
+      expect(mail.from).to eq(['from@example.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('A book was returned from your account.')
     end
   end
 end
