e7d450c810b40067ce573215a0f4cc411a9d0c46
Shivam Kumar <shivam.kumar@mountblue.tech>
2021-05-03 13:43:04 +0530

    Add examples to test mailer

--- spec/mailers/reminder_mailer_spec.rb
+++ spec/mailers/reminder_mailer_spec.rb
@@ -1,18 +1,20 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe ReminderMailer, type: :mailer do
-  describe "reminder" do
-    let(:mail) { ReminderMailer.reminder }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("Reminder")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+  describe 'reminder' do
+    let(:bookissue) { create(:bookissue) }
+    let(:mail) { ReminderMailer.reminder(bookissue) }
+    it 'renders the headers' do
+      book = bookissue.book
+      user = bookissue.user
+      expect(mail.subject).to eq("Reminder to return #{book.title}")
+      expect(mail.to).to eq([user.email.to_s])
+      expect(mail.from).to eq(['from@example.com'])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it 'renders the body' do
+      book = Book.find(bookissue.book_id)
+      expect(mail.body.encoded).to match("#{book.title} was issued to your id on #{bookissue.created_at.to_date}")
     end
   end
-
 end
